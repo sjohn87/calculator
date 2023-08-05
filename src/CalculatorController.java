a793b6bce07316921fc4257e9540a07934aaed1d
Ivan <senjohn.0000@gmail.com>
2023-08-05 21:35:03 +0200

    refactoring

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -6,7 +6,7 @@ public class CalculatorController {
         Calculator calculator = new Calculator();
 
 
-        System.out.println("Введите первое число:");
+        System.out.println("Введите первое число от 1 до 10:");
         int value1 = scanner.nextInt();
         calculator.setValue1(value1);
 
@@ -14,7 +14,7 @@ public class CalculatorController {
         char operator = scanner.next().charAt(0);
         calculator.setOperator(operator);
 
-        System.out.println("Введите второе число:");
+        System.out.println("Введите второе число от 1 до 10:");
         int value2 = scanner.nextInt();
         calculator.setValue2(value2);
 
@@ -32,11 +32,20 @@ class Calculator {
 
     // сетим
     void setValue1(int value1) {
-        this.value1 = value1;
+        if (value1 > 0 && value1 < 11) {
+            this.value1 = value1;
+        } else {
+            throw new IllegalArgumentException("Требуется ввести значение от 1 до 10!");
+        }
+
     }
 
     void setValue2(int value2) {
-        this.value2 = value2;
+        if (value2 > 0 && value2 < 11) {
+            this.value2 = value2;
+        } else {
+            throw new IllegalArgumentException("Требуется ввести значение от 1 до 10!");
+        }
     }
 
     void setOperator(char operator) {
@@ -46,23 +55,13 @@ class Calculator {
 
     //расчеты и вывод результата
     void calculateResult() {
-
-        switch (operator) {
-            case '+':
-                result = value1 + value2;
-                break;
-            case '-':
-                result = value1 - value2;
-                break;
-            case '*':
-                result = value1 * value2;
-                break;
-            case '/':
-                result = value1 / value2;
-                break;
-            default:
-                System.out.println("Не подошло ни одно условие");
-        }
+        result = switch (operator) {
+            case '+' -> value1 + value2;
+            case '-' -> value1 - value2;
+            case '*' -> value1 * value2;
+            case '/' -> value1 / value2;
+            default -> throw new IllegalStateException("Не подошло ни одно условие");
+        };
         System.out.println("Ответ: " + result);
     }
 
