718feccd82440f65424e17a99b407311601242c4
Ivan <senjohn.0000@gmail.com>
2023-08-08 20:22:42 +0200

    refactoring

--- src/CalculatorController.java
+++ src/CalculatorController.java
@@ -16,40 +16,38 @@ class Calculator {
     private int value1;
     private int value2;
     private char operator;
-    private int result;
 
     Scanner scanner = new Scanner(System.in);
 
     // сетим
     void setValue1() {
-        System.out.println("Введите первое число от 1 до 10:");
+        System.out.println("Введите первое целое число от 1 до 10:");
         value1 = scanner.nextInt();
-        if (value1 > 0 && value1 < 11) {
-           this.value1 = value1;
-        } else {
+        if (value1 < 0 || value1 > 10) {
             throw new IllegalArgumentException("Требуется ввести значение от 1 до 10!");
         }
     }
 
     void setValue2() {
-        System.out.println("Введите второе число от 1 до 10:");
+        System.out.println("Введите второе целое число от 1 до 10:");
         value2 = scanner.nextInt();
-        if (value2 > 0 && value2 < 11) {
-            this.value2 = value2;
-        } else {
+        if (value2 < 0 || value2 > 10) {
             throw new IllegalArgumentException("Требуется ввести значение от 1 до 10!");
         }
     }
 
     void setOperator() {
         System.out.println("Введите арифметическое действие:");
-        char operator = scanner.next().charAt(0);
+        operator = scanner.next().charAt(0);
+        if (operator != '+' || operator != '-' || operator != '*' || operator != '/') {
+            throw new IllegalArgumentException("Требуется ввести знак арифметическго действия!");
+        }
     }
 
 
     //расчеты и вывод результата
     void calculateResult() {
-        result = switch (operator) {
+        int result = switch (operator) {
             case '+' -> value1 + value2;
             case '-' -> value1 - value2;
             case '*' -> value1 * value2;
